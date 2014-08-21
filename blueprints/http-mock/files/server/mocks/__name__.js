62f6094913d250d5a4bc1cc070f27b701e0f8804
Ilya Radchenko <ilya@burstcreations.com>
2014-08-21 11:18:23 -0400

    Add singular mock resource setup

--- blueprints/http-mock/files/server/mocks/__name__.js
+++ blueprints/http-mock/files/server/mocks/__name__.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var <%= camelizedModuleName %>Router = express.Router();
 
+// Plural <%= camelizedModuleName %> endpoints
 <%= camelizedModuleName %>Router.route('/')
   .get(function(req, res) {
-    res.send({<%= dasherizedModuleName %>:[]});
+    res.json({ <%= camelizedModuleName %>: [] });
   });
+  
+// Singular <%= camelizedModuleName %> endpoints
+<%= camelizedModuleName %>Router.route('/:id')
+  .get(function (req, res) {
+    var id = req.params.id;
+    
+    res.json({ <%= camelizedModuleName %>: { id: id } });
+  });
+  
 
 module.exports = function(app) {
   app.use('/api<%= path %>', <%= camelizedModuleName %>Router);
