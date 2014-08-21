54af6c8c9d04d6d329f6aa20f1d2b0a4eec0c4b1
Ilya Radchenko <ilya@burstcreations.com>
2014-08-21 10:52:00 -0400

    Restructure mock route template

--- blueprints/http-mock/files/server/mocks/__name__.js
+++ blueprints/http-mock/files/server/mocks/__name__.js
@@ -1,8 +1,10 @@
+var express = require('express');
+var <%= camelizedModuleName %>Router = express.Router();
+
+<%= camelizedModuleName %>Router.get('/', function(req, res) {
+  res.send({<%= dasherizedModuleName %>:[]});
+});
+
 module.exports = function(app) {
-  var express = require('express');
-  var <%= camelizedModuleName %>Router = express.Router();
-  <%= camelizedModuleName %>Router.get('/', function(req, res) {
-    res.send({<%= dasherizedModuleName %>:[]});
-  });
   app.use('/api<%= path %>', <%= camelizedModuleName %>Router);
 };
